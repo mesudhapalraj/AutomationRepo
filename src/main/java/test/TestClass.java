f6f82ca17842cd013de62bb1848a59d83847b8af
Sudha Palraj <mesudhapalraj@gmail.com>
2024-07-03 21:54:45 +0100

    adding changes

--- src/main/java/test/TestClass.java
+++ src/main/java/test/TestClass.java
@@ -1,6 +1,10 @@
 package test;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import pages.PageClass;
 import utils.TestBase;
@@ -12,7 +16,21 @@ public class TestClass extends TestBase {
 public TestClass(){
 super();
 }
+    ExtentReports es;
 
+@BeforeTest
+public void config(){
+    String path=System.getProperty("user.dir")+"\\reports\\index.html";
+    ExtentSparkReporter reporter=new ExtentSparkReporter(path);
+    reporter.config().setReportName("AutomationGIT Report");
+    reporter.config().setDocumentTitle("Test Report");
+   // reporter.config().setTheme(Theme.valueOf("light"));
+
+   es=new ExtentReports();
+    es.attachReporter(reporter);
+    es.setSystemInfo("Tester","Raja");
+
+}
 @BeforeMethod
 public void setUp(){
     initialization();
@@ -22,7 +40,9 @@ public void setUp(){
 @Test
     public void gmailSignIn(){
 
+    es.createTest("GitDemo Report");
     page.clickSignInBtn();
     System.out.println("feature/test");
+    es.flush();
 }
 }
